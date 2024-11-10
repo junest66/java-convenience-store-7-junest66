e1c7483d43c4206bb59c6458647f323a23081eb2
junest66 <chlwndks333@gmail.com>
2024-11-10 20:25:02 +0900

    feat(InputView): 더 구매 할건지 여부를 물어보는 입력 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     public static final String PROMOTION_ADDITIONAL_ITEM_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     public static final String NON_PROMOTION_ITEM_MESSAGE = "현재 %s은(는) %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
     public static final String MEMBERSHIP_ASK_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String EXTRA_PURCHASE_ASK_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public static String readItem() {
         System.out.println(PURCHASE_ASK_MESSAGE);
@@ -42,5 +43,10 @@ public class InputView {
         System.out.println(MEMBERSHIP_ASK_MESSAGE);
         String input = Console.readLine();
         return YesOrNo.of(input).equals(YesOrNo.YES);
+    }
+
+    public static String askForAdditionalPurchase() {
+        System.out.println(EXTRA_PURCHASE_ASK_MESSAGE);
+        return Console.readLine();
     }
 }
