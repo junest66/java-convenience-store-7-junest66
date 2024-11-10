7a098d9bc16793332d20f6887ee8917cf895ec22
junest66 <chlwndks333@gmail.com>
2024-11-10 15:48:53 +0900

    test(ParsePromotionTest): Promotion 파일을 프로모션 객체로 파싱하는 기능 테스트 추가

---
+++ src/test/java/store/util/parse/ParsePromotionTest.java
@@ -0,0 +1,29 @@
+package store.util.parse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import store.entity.Promotion;
+
+class ParsePromotionTest {
+    private static final List<String> promotionData = List.of(
+            "name,buy,get,start_date,end_date",
+            "탄산2+1,2,1,2024-01-01,2024-12-31",
+            "MD추천상품,1,1,2024-01-01,2024-12-31",
+            "반짝할인,1,1,2024-11-01,2024-11-30"
+    );
+
+    @Test
+    void 프로모션_파싱_테스트() {
+        Map<String, Promotion> promotions = ParsePromotion.parse(promotionData);
+        Map<String, Promotion> expectedPromotions = Map.of(
+                "탄산2+1", new Promotion("탄산2+1", 2, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31)),
+                "MD추천상품", new Promotion("MD추천상품", 1, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31)),
+                "반짝할인", new Promotion("반짝할인", 1, 1, LocalDate.of(2024, 11, 1), LocalDate.of(2024, 11, 30))
+        );
+        assertThat(promotions).containsExactlyInAnyOrderEntriesOf(expectedPromotions);
+    }
+}
