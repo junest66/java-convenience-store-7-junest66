15945fbec756e4c91c8bd556a36847f05cf758c7
junest66 <chlwndks333@gmail.com>
2024-11-10 20:24:35 +0900

    feat(InputView): 멤버십 할인을 받을 건지 물어보는 입력 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String PURCHASE_ASK_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     public static final String PROMOTION_ADDITIONAL_ITEM_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     public static final String NON_PROMOTION_ITEM_MESSAGE = "현재 %s은(는) %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    public static final String MEMBERSHIP_ASK_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
 
     public static String readItem() {
         System.out.println(PURCHASE_ASK_MESSAGE);
@@ -35,5 +36,11 @@ public class InputView {
                     return YesOrNo.of(input).equals(YesOrNo.NO);
                 })
                 .toList();
+    }
+
+    public static boolean askForMembership() {
+        System.out.println(MEMBERSHIP_ASK_MESSAGE);
+        String input = Console.readLine();
+        return YesOrNo.of(input).equals(YesOrNo.YES);
     }
 }
