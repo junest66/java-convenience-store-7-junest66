2df1e78af9c33c4fec6b6b7d05abc0872280c6cc
junest66 <chlwndks333@gmail.com>
2024-11-10 20:23:28 +0900

    feat(InputView): 더 갖고오면 무료로 받을 수 있는 프로모션 갯수 어떻게 할건지 물어보는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,12 +1,27 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import store.constant.YesOrNo;
+import store.dto.ProductItem;
 
 public class InputView {
     private static final String PURCHASE_ASK_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    public static final String PROMOTION_ADDITIONAL_ITEM_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     public static String readItem() {
         System.out.println(PURCHASE_ASK_MESSAGE);
         return Console.readLine();
+    }
+
+    public static List<ProductItem> askForPromotionItems(List<ProductItem> additionalItems) {
+        return additionalItems.stream()
+                .peek(additionalItem -> System.out.println(String.format(PROMOTION_ADDITIONAL_ITEM_MESSAGE,
+                        additionalItem.productName(), additionalItem.quantity())))
+                .filter(additionalItem -> {
+                    String input = Console.readLine();
+                    return YesOrNo.of(input).equals(YesOrNo.YES);
+                })
+                .toList();
     }
 }
