9742a87da766e9dae45d1a0ee41b4574020dc9ca
junest66 <chlwndks333@gmail.com>
2024-11-10 15:11:33 +0900

    refactor(Promotion): Promotion 오늘 날짜를 가져오는 객체를 주입받는 형태로 변경

--- src/main/java/store/entity/Promotion.java
+++ src/main/java/store/entity/Promotion.java
@@ -1,6 +1,5 @@
 package store.entity;
 
-import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 
 public class Promotion {
@@ -9,17 +8,19 @@ public class Promotion {
     private final int get;
     private final LocalDate startDate;
     private final LocalDate endDate;
+    private final DateProvider dateProvider;
 
-    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate, DateProvider dateProvider) {
         this.name = name;
         this.buy = buy;
         this.get = get;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.dateProvider = dateProvider;
     }
 
     public boolean isPromotionValid() {
-        LocalDate currentDate = DateTimes.now().toLocalDate();
+        LocalDate currentDate = dateProvider.now();
         return !currentDate.isBefore(startDate) && !currentDate.isAfter(endDate);
     }
 
